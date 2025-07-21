4e2564ccc25ee5005ef8a2f89165405b07fac7a0
Mladen Ivkovic <mivkov@protonmail.com>
2025-07-21 17:04:22 +0100

    added header guards and writing out API

--- py/utils.py
+++ py/utils.py
@@ -2,6 +2,8 @@
 
 import os
 from typing import Union
+import subprocess
+import datetime
 
 
 def verify_file_exists(fname: str, message: str = ""):
@@ -50,3 +52,31 @@ def check_output_directory(outdir: Union[str, list], verbose: bool = True):
         os.makedirs(outdir)
 
     return outdir
+
+def get_git_hash() -> str:
+    """
+    Get the git hash of the latest commit.
+    If git is unavailable or command fails for any reason, return current time instead.
+    """
+
+    git_hash_cmd="git rev-parse --verify HEAD"
+    try:
+        githash = subprocess.run(
+                git_hash_cmd,
+                shell=True,
+                check=True,
+                capture_output=True,
+            )
+        gitstdout = githash.stdout
+        if isinstance(gitstdout, bytes):
+            gitstdout = gitstdout.decode("utf8")
+        hashstr = gitstdout.strip()
+    except subprocess.CalledProcessError:
+        print("Something went wrong when trying to obtain hash of latest git commit.")
+        print(f"Command that failed was '{git_hash_cmd}'")
+        print("Using current timestamp instead.")
+        hashstr = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S_%f")
+
+    return hashstr
+
+
