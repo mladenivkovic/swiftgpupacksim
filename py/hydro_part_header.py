2700107aff96756c1e18c6a09ab8017367096d2f
Mladen Ivkovic <mivkov@protonmail.com>
2025-07-21 16:32:34 +0100

    rudimentary particle header generation for single particle struct finished

--- py/hydro_part_header.py
+++ py/hydro_part_header.py
@@ -1,62 +1,55 @@
 #!/usr/bin/env python3
 
+from parse_yaml import FieldEntry
+from templates import jinja_generate_hydro_part_h
 
-def generate_single_part_struct(part_d: dict, verbose: bool=False):
+
+def generate_hydro_part_header(part_d: dict, swift_header: bool = True, verbose: bool=False):
     """
-    Generate a hydro_part.h with a single struct part
+    Generate a hydro_part.h with a single struct part.
+
+    Parameters
+    ----------
+
+    part_d: dict
+        dict containing the read-in particle struct fields
+
+    swift_header: bool
+        if True, generate headers compatible with swift, not swiftgpupacksim
+
+    verbose: bool
+        if True, be talkative
     """
 
     if verbose:
         print("Generating single particle struct")
-    print(part_d)
-    #  print(type(part_d))
-    #  print(part_d.keys())
-    #  print(len(part_d.keys()))
 
-    struct_name = list(part_d.keys())[0]
-    #  print(struct_name)
+    if len(part_d.keys()) > 1:
+        raise NotImplementedError("Split structs not implemented yet")
 
-    #  print(part_d["part"])
-    #  print(part_d["part"]["x"])
+    part_struct_d = {}
 
-    print("loop")
-    for field in list(part_d[struct_name].keys()):
-        field_type = "float"
-        field_size = 1
-        #  "field" here is x, h, m...
-        field_props = part_d[struct_name][field]
-        if field_props is None:
-            print("Found float:", field)
-        else:
-            try:
-                field_type = field_props["type"]
-                print("Found type:", field_type)
-            except KeyError:
-                pass
+    for struct_name in part_d.keys():
 
-            try:
-                field_size = field_props["size"]
-                print("Found size:", field_size)
-            except KeyError:
-                pass
+        declarations = []
+        apis = []
 
+        for field in list(part_d[struct_name].keys()):
+            # parse the field specification
+            field_props = part_d[struct_name][field]
+            field_entry = FieldEntry(field, field_props, verbose)
+            # get the declaration C-code
+            decl = field_entry.generate_declaration()
+            declarations.append(decl)
+            # get the API C-code
+            api = field_entry.generate_API()
+            apis.append(api)
 
-    #      print(field.type)
-    #      print(field)
-    #      print(type(field))
-    #
+        # store the parsed data
+        part_struct_d[struct_name] = {"API": apis, "DECLARATIONS": declarations}
 
+    jinja_generate_hydro_part_h(part_struct_d, swift_header=swift_header, verbose=verbose)
 
     return
 
 
-def generate_split_part_structs(part_d: dict, verbose: bool=False):
-    """
-    Generate a hydro_part.h with a single struct part
-    """
-
-    if verbose:
-        print("Generating particle composed of several structs")
-
-
-    return
