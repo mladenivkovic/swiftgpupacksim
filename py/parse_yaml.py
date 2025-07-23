010316b3f8330f953b9807c0977d213d039de13d
Mladen Ivkovic <mivkov@protonmail.com>
2025-07-23 16:02:10 +0100

    added (recursive) struct generation

--- py/parse_yaml.py
+++ py/parse_yaml.py
@@ -12,6 +12,7 @@ _allowed_field_descriptors = [
     "size",
     "doc",
     "documentation",
+    "contents"
 ]
 
 # List of all permissible particle field data types.
@@ -22,6 +23,7 @@ _allowed_field_data_types = [
     "long long",
     "float",
     "double",
+    "struct"
 ]
 
 
@@ -34,16 +36,42 @@ class FieldEntry(object):
     # jinja environment used to load and generate templates
     jinja_env = init_jinja_env()
 
-    def __init__(self, field_name, field_props: Union[dict, None], verbose=False):
+    def __init__(self, field_name: str, field_props: Union[dict, None], verbose=False, recursion_level=0):
+        """
+        Parameters
+        ----------
 
-        self.field_name = field_name
-        self.field_props = field_props
+        field_name: str
+            name of the particle field to be extracted.
+
+        field_props: dict or None
+            a dict of the read-in yaml file specifications for this
+            field. None if none were found.
+
+        verbose: bool
+            If true, be talkative about what we find.
+
+        recursion_level: int
+            Current recursion level. Used when handling structs
+            defined within the parent struct and unions as an emergency
+            exit in case something goes wrong. Also used to determine the
+            indentation level of the contents of the sub-struct/union contents.
+
+        """
+
+        if recursion_level > 20:
+            raise ValueError("We recursed 20 times, this seems excessive.")
+
+        self.name = field_name
+        self.props = field_props
         self.verbose = verbose
+        self.recursion_level = recursion_level
 
         # initialise default values
-        self.field_type = "float"
-        self.field_size = 1
+        self.type = "float"
+        self.size = 1
         self.documentation = None
+        self.sub_entries = []
 
         # Now read what was written.
         self._extract_field_data()
@@ -55,46 +83,63 @@ class FieldEntry(object):
         """
 
         if self.verbose:
-            print(f"Found field '{self.field_name}'")
+            print(f"Found field '{self.name}'")
 
-        if self.field_props is None:
+        if self.props is None:
             if self.verbose:
                 # default is set in initialiser
-                print(f"  type: default [{self.field_type}]")
-        else:
-            # first, validate that we have no unknown fields.
-            for key in self.field_props.keys():
-                if key not in _allowed_field_descriptors:
-                    print(f"WARNING: Field '{self.field_name}':")
-                    print(f"WARNING: Unknown field descriptor '{key}'. Skipping it.")
-            # Now see what's around.
-            try:
-                self.field_type = self.field_props["type"]
-                if self.verbose:
-                    print("  type:", self.field_type)
-            except KeyError:
-                pass
+                print(f"  type: default [{self.type}]")
 
-            try:
-                self.field_size = self.field_props["size"]
-                if self.verbose:
-                    print("  size:", self.field_size)
-            except KeyError:
-                pass
+            #nothing else to do.
+            return
 
+        # We have props to sort through.
+        # First, validate that we have no unknown fields.
+        for key in self.props.keys():
+            if key not in _allowed_field_descriptors:
+                print(f"WARNING: Field '{self.name}':")
+                print(f"WARNING: Unknown field descriptor '{key}'. Skipping it.")
+
+        # Now see what's around.
+        try:
+            self.type = self.props["type"]
+            if self.verbose:
+                print("  type:", self.type)
+        except KeyError:
+            pass
+
+        try:
+            self.size = self.props["size"]
+            if self.verbose:
+                print("  size:", self.size)
+        except KeyError:
+            pass
+
+        try:
+            self.documentation = self.props["doc"]
+            if self.verbose:
+                print("  documentation:", self.documentation)
+        except KeyError:
+            pass
+        try:
+            self.documentation = self.props["documentation"]
+            if self.verbose:
+                print("  documentation:", self.documentation)
+        except KeyError:
+            pass
+
+        # todo: struct* ?
+        if self.type == "struct":
             try:
-                self.documentation = self.field_props["doc"]
-                if self.verbose:
-                    print("  documentation:", self.documentation)
+                contents = self.props["contents"]
             except KeyError:
-                pass
-            try:
-                self.documentation = self.field_props["documentation"]
-                if self.verbose:
-                    print("  documentation:", self.documentation)
-            except KeyError:
-                pass
+                raise KeyError("'struct' definition within struct needs contents.")
+            for key in contents.keys():
+                new_entry = FieldEntry(key, contents[key], verbose=self.verbose, recursion_level=self.recursion_level + 1)
+                self.sub_entries.append(new_entry)
+
         return
+
 
     def _get_field_dict(self, indent_level: int = 0) -> dict:
         """
@@ -106,13 +151,15 @@ class FieldEntry(object):
         """
 
         d = {
-            "NAME": self.field_name,
-            "SIZE": self.field_size,
+            "NAME": self.name,
+            "SIZE": self.size,
             "DOC": self.documentation,
-            "TYPE": self.field_type,
+            "TYPE": self.type,
             "INDENT_LEVEL": indent_level,
             "HAS_DOC": self.documentation is not None,
-            "IS_ARRAY": self.field_size > 1,
+            "IS_ARRAY": self.size > 1,
+            "IS_UNION": self.type == "union",
+            "IS_STRUCT": self.type == "struct",
         }
 
         return d
@@ -127,9 +174,19 @@ class FieldEntry(object):
         """
 
         params_dict = self._get_field_dict(indent_level)
-        templ = self.jinja_env.get_template("field_declaration.jinja.template")
 
-        decl = templ.render(params_dict)
+        if params_dict["IS_STRUCT"] or params_dict["IS_UNION"]:
+            decl_subfields = ""
+            for entry in self.sub_entries:
+                new_entry_c_code = entry.generate_declaration(indent_level + 1)
+                decl_subfields = "".join((decl_subfields, new_entry_c_code))
+            params_dict["SUB_FIELD_DECLARATIONS"]=decl_subfields
+            templ = self.jinja_env.get_template("struct_field_declaration.jinja.template")
+            decl = templ.render(params_dict)
+
+        else:
+            templ = self.jinja_env.get_template("field_declaration.jinja.template")
+            decl = templ.render(params_dict)
 
         return decl
 
