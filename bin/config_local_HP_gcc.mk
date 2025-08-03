fc90f4427c57c5b5037f4c868304f308eae80f26
Mladen Ivkovic <mivkov@protonmail.com>
2025-08-03 21:12:40 +0100

    remove comment

--- bin/config_local_HP_gcc.mk
+++ bin/config_local_HP_gcc.mk
@@ -3,7 +3,6 @@ $(warning READING IN COMPILER SETTINGS FROM config_local_HP_gcc.mk)
 
 CC = gcc
 NVCC = nvcc
-# -Wno-unused-but-set-variable -Wno-unused-variable
 
 CFLAGS = -fno-lto -I//opt/cuda/include -fvar-tracking-assignments -O3 -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math -funroll-loops -march=core2 -mavx512vbmi -pthread  -Wall -Wextra -Wshadow -Wstrict-prototypes
 CFLAGS_DEBUG = -g -O0  -fno-lto -I//opt/cuda/include -gdwarf -fvar-tracking-assignments -fomit-frame-pointer -malign-double -fstrict-aliasing -ffast-math -funroll-loops -march=core2 -mavx512vbmi -pthread  -Wall -Wextra -Wshadow -Wstrict-prototypes
