fee784347a8b6f9f517878226816b8d5aac930fa
Mladen Ivkovic <mivkov@protonmail.com>
2025-08-03 21:27:03 +0100

    moved input data for tests into subdir

--- test/test_read_paramfile.c
+++ test/test_read_paramfile.c
@@ -13,8 +13,10 @@ int main(void) {
   struct parameters params;
   init_params(&params);
 
+  /* Fake cmdlineargs. */
   int argc = 2;
-  char *argv[] = {"swiftpacksim", "."};
+  /* First argument: fake executable name. */
+  char *argv[] = {"swiftpacksim", "input"};
   io_parse_cmdlineargs(argc, argv, &params);
   validate_params(&params);
 
