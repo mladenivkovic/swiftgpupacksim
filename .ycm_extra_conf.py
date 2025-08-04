c4ea1d7d9fbb4b3da9c57b79507a799d1bba53eb
Mladen Ivkovic <mivkov@protonmail.com>
2025-08-04 09:24:09 +0100

    ran formatting tool

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,6 +1,6 @@
 import os
 
-DIR_OF_THIS_SCRIPT = os.path.abspath( os.path.dirname( __file__ ) )
+DIR_OF_THIS_SCRIPT = os.path.abspath(os.path.dirname(__file__))
 
 
 def _add_include(path, include="include"):
@@ -57,28 +57,33 @@ except KeyError:
     COMPILER = "gcc"
 
 flags = [
-    '-Wall',
-    '-Wextra',
+    "-Wall",
+    "-Wextra",
     # THIS IS IMPORTANT! Without the '-x' flag, Clang won't know which language to
     # use when compiling headers. So it will guess. Badly. So C++ headers will be
     # compiled as C headers. You don't want that so ALWAYS specify the '-x' flag.
     # For a C project, you would set this to 'c' instead of 'c++'.
-    '-x', 'c',
-    '-Wno-unused-includes',
-    '-Wno-unused-parameter',
-    '-Wno-unused-function',
-    '-std=c11',
-    '-DHAVE_CUDA=1', # cheat for the bloody linter
-    '-DCONFIG_LOCAL_HP_ICX=1',
+    "-x",
+    "c",
+    "-Wno-unused-includes",
+    "-Wno-unused-parameter",
+    "-Wno-unused-function",
+    "-std=c11",
+    "-DHAVE_CUDA=1",  # cheat for the bloody linter
+    "-DCONFIG_LOCAL_HP_ICX=1",
 ]
 
 
 include = [
-    "-I", DIR_OF_THIS_SCRIPT,       # add config.h
-    "-I", os.path.join(DIR_OF_THIS_SCRIPT, "src"),
-    "-I", os.path.join(DIR_OF_THIS_SCRIPT, "src/cuda"),
-    "-I", os.path.join(DIR_OF_THIS_SCRIPT, "src/swift_placeholders"),
-        ]
+    "-I",
+    DIR_OF_THIS_SCRIPT,  # add config.h
+    "-I",
+    os.path.join(DIR_OF_THIS_SCRIPT, "src"),
+    "-I",
+    os.path.join(DIR_OF_THIS_SCRIPT, "src/cuda"),
+    "-I",
+    os.path.join(DIR_OF_THIS_SCRIPT, "src/swift_placeholders"),
+]
 
 try:
     CUDA_PATH = os.environ["CUDA_PATH"]
@@ -94,18 +99,16 @@ except KeyError:
         pass
 
 
+def Settings(**kwargs):
 
-
-def Settings( **kwargs ):
-
-    if kwargs[ 'language' ] == 'cfamily':
+    if kwargs["language"] == "cfamily":
 
         #  all_defines = _parse_config_h()
         all_defines = []
         all_includes = include
 
         # add current directory of the file
-        fname = kwargs[ "filename" ]
+        fname = kwargs["filename"]
         absfname = os.path.abspath(fname)
         fdir = os.path.dirname(absfname)
         all_includes.append("-I")
@@ -118,5 +121,5 @@ def Settings( **kwargs ):
         final_flags = flags + all_defines + all_includes
 
         return {
-            'flags': final_flags,
-          }
+            "flags": final_flags,
+        }
--- test/test_header_output.c
+++ test/test_header_output.c
@@ -1,10 +1,10 @@
 /* A minimal program to verify that the generated headers are valid c code. */
 
-#include <stdio.h>
-
 #include "hydro_part.h"
 
-int main(void){
+#include <stdio.h>
+
+int main(void) {
 
   printf("Running minimal test with generated header. ");
   printf("Size of part is: %ld\n", sizeof(struct part));
