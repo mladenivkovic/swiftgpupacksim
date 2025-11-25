c2a9a1b6def00d31c18c38c2d9edad90a516eda5
Mladen Ivkovic <mivkov@protonmail.com>
2025-11-25 21:27:08 +0000

    add run_all_tests.sh

---
+++ test/run_all_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+make check
+
+# Alternatively, do it manually.
+# Tests should report "Test passed." with exit code 0 if successful
+
+# make
+# ./test_cmdlineargs.o
+# ./test_read_data_file.o
+# ./test_read_paramfile.o
+#
+# ./pytests.sh
