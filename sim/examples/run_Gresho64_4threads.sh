6de5ab9108fa36a036dc7a44ebbb650410cf1504
Mladen Ivkovic <mivkov@protonmail.com>
2025-12-17 17:52:58 +0100

    adapt run scripts

--- sim/examples/run_Gresho64_4threads.sh
+++ sim/examples/run_Gresho64_4threads.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export OMP_NUM_THREADS=4
-likwid-pin -c M1:0-4 ../swiftgpupack ../../data/IntelXeonGold5218_Gresho64_4threads
+export OMP_PROC_BIND=true
+../swiftgpupack ../../data/IntelXeonGold5218_Gresho64_4threads --noflush -s 1
 
 
