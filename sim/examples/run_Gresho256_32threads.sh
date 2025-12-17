db326d30b95ac03aafa37e1b940e053e03f57ae5
Mladen Ivkovic <mivkov@protonmail.com>
2025-12-17 17:52:58 +0100

    adapt run scripts

--- sim/examples/run_Gresho256_32threads.sh
+++ sim/examples/run_Gresho256_32threads.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export OMP_NUM_THREADS=32
+export OMP_PROC_BIND=true
 likwid-pin -c N:0-32 ../swiftgpupack ../../data/IntelXeonGold5218_Gresho256_32threads
 
 
--- sim/examples/run_Gresho64_singlethread.sh
+++ sim/examples/run_Gresho64_singlethread.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export OMP_NUM_THREADS=1
+export OMP_PROC_BIND=true
 likwid-pin -c M0:1 ../swiftgpupack ../../data/IntelXeonGold5218_Gresho64_1thread
 
 
