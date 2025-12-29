55385c2ea8b9408ac4368c2fd2c5f1e2a4dc7f8f
Mladen Ivkovic <mivkov@protonmail.com>
2025-12-29 21:44:59 +0100

    added SPHENIX_pack_gradient

--- sim/src/hydro_part.h
+++ sim/src/hydro_part.h
@@ -36,6 +36,12 @@
 #include "realisations/soa/hydro_part.h"
 #elif defined(SPHENIX_UPSTREAM_PARTICLE)
 #include "realisations/upstream/hydro_part.h"
+#elif defined(SPHENIX_PACK_GRADIENT_PARTICLE)
+#include "realisations/pack_gradient/hydro_part.h"
+#elif defined(SPHENIX_PACK_FORCE_PARTICLE)
+#include "realisations/pack_force/hydro_part.h"
+#elif defined(SPHENIX_SOA_MANUAL_PARTICLE)
+#include "realisations/soa_manual/hydro_part.h"
 #else
 #pragma error "Unknown memory layout realisation"
 #endif
