90c598f02b714db090b1397be478a01dab84e792
Mladen Ivkovic <mivkov@protonmail.com>
2025-12-30 11:51:45 +0000

    use --config-cache

--- sim/make_all_struct_alignments.sh
+++ sim/make_all_struct_alignments.sh
@@ -21,12 +21,12 @@ fi
 
 for layout in aos soa soa-modified upstream pack-gradient pack-force pack-shared; do
   for align in 1 2 4 8 16 32 64; do
-    ./configure --with-particle-memory-layout="$layout" $EXTRA_CONFFLAGS
+    ./configure --config-cache --with-particle-memory-layout="$layout" $EXTRA_CONFFLAGS
     make clean
     make -j
     mv ./swiftgpupack ./swiftgpupack_"$layout"_struct_align_"$align"
 
-    ./configure --with-particle-memory-layout="$layout" $EXTRA_CONFFLAGS --disable-vectorization
+    ./configure --config-cache --with-particle-memory-layout="$layout" $EXTRA_CONFFLAGS --disable-vectorization
     make clean
     make -j
     mv ./swiftgpupack ./swiftgpupack_"$layout"_struct_align_"$align"_novector
