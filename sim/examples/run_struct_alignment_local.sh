de1e9da87538a4ffd0c2f8e81387261402559290
Mladen Ivkovic <mivkov@protonmail.com>
2025-12-30 10:30:44 +0100

    added new layouts to scritps

--- sim/examples/run_struct_alignment_local.sh
+++ sim/examples/run_struct_alignment_local.sh
@@ -16,19 +16,30 @@ for align in 1 2 4 8 16 32 64; do
     mkdir -p $NODE
     mkdir -p $NODE/"$dir"_struct_align"$align"
     mkdir -p $NODE/"$dir"_struct_align"$align"_noflush
+    mkdir -p $NODE/"$dir"_struct_align"$align"_novector
+    mkdir -p $NODE/"$dir"_struct_align"$align"_noflush_novector
 
     NTHREADS=4
 
-    for ex in ../swiftgpupack_aos ../swiftgpupack_soa ../swiftgpupack_soa-modified ../swiftgpupack_upstream; do
+    for ex in ../swiftgpupack_aos ../swiftgpupack_soa ../swiftgpupack_soa-modified ../swiftgpupack_upstream ../swiftgpupack_pack-gradient ../swiftgpupack_pack-force ../swiftgpupack_pack-shared; do
 
       "$ex"_struct_align_"$align" ../../data/$dir
       mv results_*.csv $NODE/"$dir"_struct_align"$align"/
 
+      "$ex"_struct_align_"$align"_novector ../../data/$dir
+      mv results_*.csv $NODE/"$dir"_struct_align"$align"_novector/
+
       "$ex"_struct_align_"$align" ../../data/$dir --noflush
       mv results_*.csv $NODE/"$dir"_struct_align"$align"_noflush/
+
+      "$ex"_struct_align_"$align"_novector ../../data/$dir --noflush
+      mv results_*.csv $NODE/"$dir"_struct_align"$align"_noflush_novector/
 
     done
 
   done
 
 done
+
+
+python3 ../../utils/plot_struct_alignments.py
