7f3ae223b5b94b50b2caa4dcdea273cd4fa4aa98
Mladen Ivkovic <mivkov@protonmail.com>
2026-04-16 15:07:03 +0100

    minor tweaks run_loop_variants_local

--- sim/examples/run_loop_variants_local.sh
+++ sim/examples/run_loop_variants_local.sh
@@ -11,10 +11,10 @@ rm -f results_*.csv
 
 export OMP_PROC_BIND=true
 
-# for part_access in "part-struct" "global-var" "explicit-var"; do
+for part_access in "part-struct" "global-var" "explicit-var"; do
 # for part_access in "part-struct"; do
 # for part_access in "explicit-var"; do
-for part_access in "global-var"; do
+# for part_access in "global-var"; do
   for loop in "none" "by-struct" "by-element"; do
 
     # Experiment reproduction directories
@@ -27,40 +27,47 @@ for part_access in "global-var"; do
       mkdir -p $NODE/"$dir"_"$part_access"_"$loop"_vector_noflush
       mkdir -p $NODE/"$dir"_"$part_access"_"$loop"_packed
       mkdir -p $NODE/"$dir"_"$part_access"_"$loop"_packed_noflush
-      mkdir -p $NODE/"$dir"_"$part_access"_"$loop"_vector_packed
-      mkdir -p $NODE/"$dir"_"$part_access"_"$loop"_vector_packed_noflush
+      mkdir -p $NODE/"$dir"_"$part_access"_"$loop"_packed_vector
+      mkdir -p $NODE/"$dir"_"$part_access"_"$loop"_packed_vector_noflush
 
       NTHREADS=4
+      export OMP_NUM_THREADS=$NTHREADS
 
-      # for ex in ../swiftgpupack_aos ../swiftgpupack_soa ../swiftgpupack_soa-modified ../swiftgpupack_upstream ../swiftgpupack_pack-gradient ../swiftgpupack_pack-force ../swiftgpupack_pack-shared; do
       for layout in aos soa upstream pack-gradient pack-force pack-shared; do
 
         ex=../swiftgpupack_"$layout"_"$part_access"_"$loop"
 
-        echo "$ex" ../../data/$dir
-        "$ex" ../../data/$dir
+        echo running "$ex" ../../data/$dir -s 1
+        "$ex" ../../data/$dir -s 1
         mv results_*.csv $NODE/"$dir"_"$part_access"_"$loop"/
 
-        "$ex" ../../data/$dir --noflush
+        echo running "$ex" ../../data/$dir --noflush -s 1
+        "$ex" ../../data/$dir --noflush -s 1
         mv results_*.csv $NODE/"$dir"_"$part_access"_"$loop"_noflush/
 
-        "$ex"_vector ../../data/$dir
+        echo running "$ex"_vector ../../data/$dir -s 1
+        "$ex"_vector ../../data/$dir -s 1
         mv results_*.csv $NODE/"$dir"_"$part_access"_"$loop"_vector/
 
-        "$ex"_vector ../../data/$dir --noflush
+        echo running "$ex"_vector ../../data/$dir --noflush -s 1
+        "$ex"_vector ../../data/$dir --noflush -s 1
         mv results_*.csv $NODE/"$dir"_"$part_access"_"$loop"_vector_noflush/
 
-        "$ex"_packed ../../data/$dir
+        echo running "$ex"_packed ../../data/$dir -s 1
+        "$ex"_packed ../../data/$dir -s 1
         mv results_*.csv $NODE/"$dir"_"$part_access"_"$loop"_packed/
 
-        "$ex"_packed ../../data/$dir --noflush
+        echo running "$ex"_packed ../../data/$dir --noflush -s 1
+        "$ex"_packed ../../data/$dir --noflush -s 1
         mv results_*.csv $NODE/"$dir"_"$part_access"_"$loop"_packed_noflush/
 
-        "$ex"_vector_packed ../../data/$dir
-        mv results_*.csv $NODE/"$dir"_"$part_access"_"$loop"_vector_packed/
+        echo running "$ex"_vector_packed ../../data/$dir -s 1
+        "$ex"_vector_packed ../../data/$dir -s 1
+        mv results_*.csv $NODE/"$dir"_"$part_access"_"$loop"_packed_vector/
 
-        "$ex"_vector_packed ../../data/$dir --noflush
-        mv results_*.csv $NODE/"$dir"_"$part_access"_"$loop"_vector_packed_noflush/
+        echo running "$ex"_vector_packed ../../data/$dir --noflush -s 1
+        "$ex"_vector_packed ../../data/$dir --noflush -s 1
+        mv results_*.csv $NODE/"$dir"_"$part_access"_"$loop"_packed_vector_noflush/
 
       done
     done
@@ -68,4 +75,6 @@ for part_access in "global-var"; do
 done
 
 
-python3 ../../utils/plot_struct_alignments.py -l local
+# python3 ../../utils/plot_loop_splitting_all_in_single_plot.py -l local --png --flush-vector
+# python3 ../../utils/plot_loop_splitting_all_in_single_plot.py -l local --png --packed-vector
+# python3 ../../utils/plot_loop_splitting_all_in_single_plot.py -l local --png --packed-flush
