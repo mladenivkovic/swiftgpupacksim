75b0539474d5dcece71f270ca59686225c478c85
Mladen Ivkovic <mivkov@protonmail.com>
2026-05-01 01:51:33 +0100

    modifying main make all script

--- sim/make_all_loop_splitting_variants.sh
+++ sim/make_all_loop_splitting_variants.sh
@@ -6,6 +6,10 @@ if [[ ! -f ./configure ]]; then
   ./autogen.sh
 fi
 
+# NODE=dine2
+NODE=gn003
+# NODE=milan
+
 EXTRA_CONFFLAGS="--config-cache --enable-no-cuda-mallochost"
 
 if [ $# -gt 0 ]; then
@@ -22,37 +26,51 @@ fi
 for part_access in "part-struct" "global-var" "explicit-var"; do
   for loop in "none" "by-struct" "by-element" "by-type" "by-struct-and-type"; do
     for layout in "aos" "soa" "upstream" "pack-gradient" "pack-force" "pack-shared"; do
-      ./configure --with-particle-memory-layout="$layout" --with-loop-split=$loop --with-particle-access=$part_access $EXTRA_CONFFLAGS
-      make clean
-      echo
-      echo $loop $layout
-      echo
-      make -j
-      mv ./swiftgpupack ./swiftgpupack_"$layout"_"$part_access"_"$loop"
 
-      ./configure --with-particle-memory-layout="$layout" --with-loop-split=$loop --with-particle-access=$part_access --enable-vectorization-pragmas $EXTRA_CONFFLAGS
-      make clean
-      echo
-      echo $loop $layout
-      echo
-      make -j
-      mv ./swiftgpupack ./swiftgpupack_"$layout"_"$part_access"_"$loop"_vector
+      SUFFIX="$NODE"_"$layout"_"$part_access"_"$loop"
 
-      ./configure --with-particle-memory-layout="$layout" --with-loop-split=$loop --with-particle-access=$part_access --enable-packed-structs $EXTRA_CONFFLAGS
+      CFLAGS="-march=native" ./configure --with-particle-memory-layout="$layout" --with-loop-split=$loop --with-particle-access=$part_access $EXTRA_CONFFLAGS
       make clean
       echo
       echo $loop $layout
       echo
       make -j
-      mv ./swiftgpupack ./swiftgpupack_"$layout"_"$part_access"_"$loop"_packed
+      mv ./swiftgpupack ./swiftgpupack_"$SUFFIX"
+      echo
+      echo COMPILED ./swiftgpupack_"$SUFFIX"
+      echo
 
-      ./configure --with-particle-memory-layout="$layout" --with-loop-split=$loop --with-particle-access=$part_access --enable-vectorization-pragmas --enable-packed-structs $EXTRA_CONFFLAGS
+      CFLAGS="-march=native" ./configure --with-particle-memory-layout="$layout" --with-loop-split=$loop --with-particle-access=$part_access --enable-vectorization-pragmas $EXTRA_CONFFLAGS
       make clean
       echo
       echo $loop $layout
       echo
       make -j
-      mv ./swiftgpupack ./swiftgpupack_"$layout"_"$part_access"_"$loop"_vector_packed
+      echo
+      mv ./swiftgpupack ./swiftgpupack_"$SUFFIX"_vector
+      echo
+
+      CFLAGS="-march=native" ./configure --with-particle-memory-layout="$layout" --with-loop-split=$loop --with-particle-access=$part_access --enable-packed-structs $EXTRA_CONFFLAGS
+      make clean
+      echo
+      echo $loop $layout
+      echo
+      make -j
+      mv ./swiftgpupack ./swiftgpupack_"$SUFFIX"_packed
+      echo
+      echo COMPILED ./swiftgpupack_"$SUFFIX"_packed
+      echo
+
+      CFLAGS="-march=native" ./configure --with-particle-memory-layout="$layout" --with-loop-split=$loop --with-particle-access=$part_access --enable-vectorization-pragmas --enable-packed-structs $EXTRA_CONFFLAGS
+      make clean
+      echo
+      echo $loop $layout
+      echo
+      make -j
+      mv ./swiftgpupack ./swiftgpupack_"$SUFFIX"_vector_packed
+      echo
+      echo COMPILED ./swiftgpupack_"$SUFFIX"_vector_packed
+      echo
 
     done
   done
