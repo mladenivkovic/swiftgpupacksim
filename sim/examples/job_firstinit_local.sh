f2a5d8e0d1d2eb2e763a9e0a9037af605e0c9bf3
Mladen Ivkovic <mivkov@protonmail.com>
2026-05-06 15:41:20 +0100

    added first init variants

---
+++ sim/examples/job_firstinit_local.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Generate executables first using `swiftgpupacksim/sim/make_first_init_variants.sh`
+
+NODE=local
+
+set -e
+rm -f results_*.csv
+
+export OMP_PROC_BIND=true
+
+# for part_access in "part-struct" "global-var" "explicit-var"; do
+#   for loop in "none" "by-struct" "by-element" "by-type" "by-struct-and-type"; do
+for part_access in "explicit-var"; do
+  for loop in "none"; do
+
+    # Experiment reproduction directories
+    # for dir in EAGLE25_64threads Gresho256_64threads EAGLE25_32threads Gresho256_32threads; do
+    # for dir in EAGLE25_64threads Gresho256_64threads; do
+    for dir in IntelCoffeeLake_Gresho128_6threads; do
+
+      mkdir -p $NODE
+      mkdir -p $NODE/"$dir"_"$part_access"_"$loop"
+      mkdir -p $NODE/"$dir"_"$part_access"_"$loop"_firstinit
+
+      NTHREADS=1
+      if [[ "$dir" == *"_6threads"* ]]; then NTHREADS=6; fi
+      if [[ "$dir" == *"_4threads"* ]]; then NTHREADS=4; fi
+
+      export OMP_NUM_THREADS=$NTHREADS
+
+      for layout in aos soa upstream pack-gradient pack-force pack-shared; do
+
+        ex="likwid-pin --quiet -c N:0-""$((NTHREADS - 1))"" ../swiftgpupack_NOFIRSTINIT_""$NODE"_"$layout"_"$part_access"_"$loop"" -s 2 --noflush"
+        outfile=$NODE/"$dir"_"$part_access"_"$loop"/results_"$layout".csv
+        if [[ ! -f "$outfile" ]]; then
+          echo running "$ex" ../../data/$dir
+          $ex ../../data/$dir
+          mv results_"$layout".csv "$outfile"
+          echo written $outfile
+        else
+          echo found $outfile, not rerunning
+        fi
+
+        ex="likwid-pin --quiet -c N:0-""$((NTHREADS - 1))"" ../swiftgpupack_PARALLELINIT_""$NODE"_"$layout"_"$part_access"_"$loop"" -s 2"
+        outfile=$NODE/"$dir"_"$part_access"_"$loop"_firstinit/results_"$layout".csv
+        if [[ ! -f "$outfile" ]]; then
+          echo running "$ex" ../../data/$dir
+          $ex ../../data/$dir
+          mv results_"$layout".csv "$outfile"
+          echo written $outfile
+        else
+          echo found $outfile, not rerunning
+        fi
+
+      done
+    done
+  done
+done
+
---
+++ sim/make_first_init_variants.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -e
+
+if [[ ! -f ./configure ]]; then
+  ./autogen.sh
+fi
+
+NODE=local
+# NODE=dine2
+# NODE=gn003
+# NODE=mad06
+
+MYCFLAGS="-march=native"
+
+EXTRA_CONFFLAGS="--config-cache"
+
+if [[ "$NODE" == "local" ]]; then
+  EXTRA_CONFFLAGS="$EXTRA_CONFFLAGS --enable-no-cuda-mallochost"
+fi
+
+if [ $# -gt 0 ]; then
+  case "$1" in
+  -d | d | debug | DEBUG )
+    EXTRA_CONFFLAGS+=" --enable-debug --enable-debugging-checks"
+    ;;
+  *)
+    echo unknown cmdline param "'""$1""'"
+    ;;
+  esac
+fi
+
+# for part_access in "part-struct" "global-var" "explicit-var"; do
+  # for loop in "none" "by-struct" "by-element" "by-type" "by-struct-and-type"; do
+for part_access in "explicit-var"; do
+  for loop in "none"; do
+    for layout in "aos" "soa" "upstream" "pack-gradient" "pack-force" "pack-shared"; do
+
+      SUFFIX="$NODE"_"$layout"_"$part_access"_"$loop"
+
+      CFLAGS="$MYCFLAGS" ./configure --with-particle-memory-layout="$layout" --with-loop-split=$loop --with-particle-access=$part_access $EXTRA_CONFFLAGS
+      make clean
+      echo
+      echo $loop $layout
+      echo
+      make -j
+      mv ./swiftgpupack ./swiftgpupack_NOFIRSTINIT_"$SUFFIX"
+      echo
+      echo COMPILED ./swiftgpupack_NOFIRSTINIT_"$SUFFIX"
+      echo
+
+      CFLAGS="$MYCFLAGS" ./configure --with-particle-memory-layout="$layout" --with-loop-split=$loop --with-particle-access=$part_access $EXTRA_CONFFLAGS --enable-parallel-init
+      make clean
+      echo
+      echo $loop $layout
+      echo
+      make -j
+      mv ./swiftgpupack ./swiftgpupack_PARALLELINIT_"$SUFFIX"
+      echo
+      echo COMPILED ./swiftgpupack_PARALLELINIT_"$SUFFIX"
+      echo
+
+    done
+  done
+done
