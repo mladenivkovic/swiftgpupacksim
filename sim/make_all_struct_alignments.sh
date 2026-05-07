00e0c7a44662fc3c0ddd055980b3ced68f5b2060
Mladen Ivkovic <mivkov@protonmail.com>
2026-05-07 06:30:19 +0100

    auto-remove config.cache when starting up

--- sim/make_all_struct_alignments.sh
+++ sim/make_all_struct_alignments.sh
@@ -6,12 +6,27 @@ if [[ ! -f ./configure ]]; then
   ./autogen.sh
 fi
 
-EXTRA_CONFFLAGS=""
+# NODE=dine2
+NODE=gn003
+# NODE=mad06
+
+MYCFLAGS="-march=native -Wno-pass-failed"
+
+EXTRA_CONFFLAGS="--config-cache"
+
+if [[ "$NODE" == "local" ]]; then
+  EXTRA_CONFFLAGS="$EXTRA_CONFFLAGS --enable-no-cuda-mallochost"
+fi
+
+if [[ "$NODE" == "mad06" ]]; then
+  MYCFLAGS="$MYCFLAGS -Wno-unused-command-line-argument"
+fi
+
 
 if [ $# -gt 0 ]; then
   case "$1" in
   -d | d | debug | DEBUG )
-    EXTRA_CONFFLAGS="--enable-debug --enable-debugging-checks"
+    EXTRA_CONFFLAGS+=" --enable-debug --enable-debugging-checks"
     ;;
   *)
     echo unknown cmdline param "'""$1""'"
@@ -19,14 +34,17 @@ if [ $# -gt 0 ]; then
   esac
 fi
 
+
+rm -f config.cache
+
 for layout in aos soa upstream pack-gradient pack-force pack-shared; do
   for align in 1 2 4 8 16 32 64; do
-    ./configure --config-cache --with-particle-memory-layout="$layout" --with-struct-align=$align $EXTRA_CONFFLAGS
+    CFLAGS="$MYCFLAGS" ./configure --config-cache --with-particle-memory-layout="$layout" --with-struct-align=$align $EXTRA_CONFFLAGS
     make clean
     make -j
     mv ./swiftgpupack ./swiftgpupack_"$layout"_struct_align_"$align"
 
-    ./configure --config-cache --with-particle-memory-layout="$layout" --with-struct-align=$align $EXTRA_CONFFLAGS --disable-vectorization
+    CFLAGS="$MYCFLAGS" ./configure --config-cache --with-particle-memory-layout="$layout" --with-struct-align=$align $EXTRA_CONFFLAGS --disable-vectorization
     make clean
     make -j
     mv ./swiftgpupack ./swiftgpupack_"$layout"_struct_align_"$align"_novector
