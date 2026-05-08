5b9a6d6fb43a0459f591693bc9ec4de044c8161b
Mladen Ivkovic <mivkov@protonmail.com>
2026-05-08 14:10:52 +0100

    added new files for particles

--- py/make_all_particles.sh
+++ py/make_all_particles.sh
@@ -4,5 +4,7 @@
 ./generate_hydro_part.py -e -g input/SPHENIX_pack_force.yml -o ../sim/src/realisations/pack_force
 ./generate_hydro_part.py -e -g input/SPHENIX_pack_gradient.yml -o ../sim/src/realisations/pack_gradient
 ./generate_hydro_part.py -e -g input/SPHENIX_pack_shared.yml -o ../sim/src/realisations/pack_shared
+./generate_hydro_part.py -e -g input/SPHENIX_pack_force_type.yml -o ../sim/src/realisations/pack_force_type
 ./generate_hydro_part.py -e -g input/SPHENIX_SOA.yml -o ../sim/src/realisations/soa
 ./generate_hydro_part.py -e -g input/SPHENIX_upstream.yml -o ../sim/src/realisations/upstream
+./generate_hydro_part.py -e -g input/SPHENIX_random_order.yml -o ../sim/src/realisations/random_order
